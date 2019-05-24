ecc0a3356d0def41b15e3c3511cdb99cdae04372
asuciu <suciuandrei94@gmail.com>
2019-05-24 11:31:04 +0300

    Added helm chart for member-service

--- member/member-service/src/main/java/crm/entity/Member.java
+++ member/member-service/src/main/java/crm/entity/Member.java
@@ -1,5 +1,6 @@
 package crm.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
@@ -18,10 +19,12 @@ public class Member {
     @GeneratedValue
     private long id;
 
+    @Column(nullable = false, unique = true, length = 36)
     private String externalId;
 
     private String name;
 
+    @Column(nullable = false, unique = true, length = 60)
     private String email;
 
     private String password;
