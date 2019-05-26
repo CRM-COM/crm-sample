0b4a78ec0be826dd1c5c95720f26f5df3ec2c511
asuciu <suciuandrei94@gmail.com>
2019-05-26 09:14:57 +0300

    Try deploy services without kubernetes cloud stack

--- gateway/gateway-service/src/main/java/crm/GatewayApplication.java
+++ gateway/gateway-service/src/main/java/crm/GatewayApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @EnableConfigurationProperties
-@EnableDiscoveryClient
 @SpringBootApplication
 public class GatewayApplication {
     public static void main(String[] args) {
--- member/member-service/src/main/java/crm/MemberApplication.java
+++ member/member-service/src/main/java/crm/MemberApplication.java
@@ -3,12 +3,10 @@ package crm;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 import crm.config.MemberStream;
 
-@EnableDiscoveryClient
 @SpringBootApplication
 @EnableBinding({MemberStream.class})
 @EnableConfigurationProperties
