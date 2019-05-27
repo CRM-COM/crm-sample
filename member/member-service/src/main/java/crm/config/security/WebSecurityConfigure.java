c0a6b0dd089c20412b0433141156bbc07d440dbe
dhencz <dzshencz@gmail.com>
2019-05-27 23:30:33 +0300

    ignore /login

--- member/member-service/src/main/java/crm/config/security/WebSecurityConfigure.java
+++ member/member-service/src/main/java/crm/config/security/WebSecurityConfigure.java
@@ -1,6 +1,7 @@
 package crm.config.security;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -15,12 +16,13 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .csrf().disable()
-            .authorizeRequests().antMatchers(permitAll()).permitAll()
-            .anyRequest().authenticated()
-            .and()
-            .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
-            .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
+                .csrf().disable()
+                .authorizeRequests().antMatchers(permitAll()).permitAll()
+                .antMatchers(HttpMethod.POST, "/login").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .addFilterBefore(new JWTLoginFilter("/login", authenticationManager()), UsernamePasswordAuthenticationFilter.class)
+                .addFilterBefore(new JWTAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
     }
 
     @Override
@@ -29,7 +31,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
     }
 
     protected String[] permitAll() {
-        return new String[]{"/actuator/**", "/login"};
+        return new String[]{"/actuator/**"};
     }
 
     @Override
