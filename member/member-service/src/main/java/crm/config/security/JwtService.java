982ad816ca11a56461b949a9fbfba7f94106db0b
dhencz <dzshencz@gmail.com>
2019-05-28 09:28:57 +0300

    use token with Bearer

--- member/member-service/src/main/java/crm/config/security/JwtService.java
+++ member/member-service/src/main/java/crm/config/security/JwtService.java
@@ -6,46 +6,42 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
 import java.util.Date;
+import java.util.Optional;
 
 @Service
 public class JwtService {
 
-    private static long EXPIRATIONTIME = 1000 * 60 * 60 * 24 * 10; // 10 days
+    private static long EXPIRATION_TIME = 1000 * 60 * 60 * 24 * 10; // 10 days
     private static String SECRET = "ThisIsASecret";
 
     public String createToken(String externalId) {
         return Jwts.builder()
                 .setSubject(externalId)
-                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATIONTIME))
+                .setExpiration(new Date(System.currentTimeMillis() + EXPIRATION_TIME))
                 .signWith(SignatureAlgorithm.HS512, SECRET)
                 .compact();
     }
 
     public String parseToken(HttpServletRequest request) {
         var token = request.getHeader("Authorization");
-        if (token != null) {
-            return Jwts.parser()
-                    .setSigningKey(SECRET)
-                    .parseClaimsJws(token)
-                    .getBody()
-                    .getSubject();
-        }
-        return null;
+        return parseToken(token);
     }
 
     public Authentication parseTokenToAuthentication(HttpServletRequest request) {
         var token = request.getHeader("Authorization");
-        if (token != null) {
-            var externalId = Jwts.parser()
-                    .setSigningKey(SECRET)
-                    .parseClaimsJws(token)
-                    .getBody()
-                    .getSubject();
-            return new AuthenticationUser(externalId);
-        }
-        return null;
+        return Optional.ofNullable(parseToken(token)).map(AuthenticationUser::new).orElse(null);
+    }
+
+    private String parseToken(String token) {
+        if (token == null || !token.startsWith("Bearer "))
+            return null;
+
+        var trimmedToken = token.substring(7);
+        return Jwts.parser()
+                .setSigningKey(SECRET)
+                .parseClaimsJws(trimmedToken)
+                .getBody()
+                .getSubject();
     }
 }
