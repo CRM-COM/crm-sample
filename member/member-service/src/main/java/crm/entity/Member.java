a15aa1d234accde4fea244dbb53c095140388b4c
dhencz <dzshencz@gmail.com>
2019-05-31 14:12:13 +0300

    revert member change

--- member/member-service/src/main/java/crm/entity/Member.java
+++ member/member-service/src/main/java/crm/entity/Member.java
@@ -22,30 +22,14 @@ public class Member {
     @Column(nullable = false, unique = true, length = 36)
     private String externalId;
 
-    private String title;
-
-    private String forename;
-
-    private String surname;
-
-    private String nickname;
-
-    private String avatarExternalId;
-
-    private boolean isDeleted;
+    private String name;
 
     @OneToMany(mappedBy = "member", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
     private Set<MemberIdentity> memberIdentities = new HashSet<>();
 
-    public Member(long id, String externalId, String title, String forename, String surname, String nickname, String avatarExternalId, boolean isDeleted, Set<MemberIdentity> memberIdentities) {
-        this.id = id;
+    public Member(String externalId, String name) {
         this.externalId = externalId;
-        this.title = title;
-        this.forename = forename;
-        this.surname = surname;
-        this.nickname = nickname;
-        this.avatarExternalId = avatarExternalId;
-        this.isDeleted = isDeleted;
-        this.memberIdentities = memberIdentities;
+        this.name = name;
     }
+
 }
