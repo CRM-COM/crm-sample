a742870424b88ee85e08fae4f85aea617e45260b
dhencz <dzshencz@gmail.com>
2019-05-31 11:57:09 +0300

    try keycloak 4

--- member/member-service/src/main/java/crm/config/security/SecurityConfig.java
+++ member/member-service/src/main/java/crm/config/security/SecurityConfig.java
@@ -1,13 +1,37 @@
 package crm.config.security;
 
-import crm.security.WebSecurityConfigure;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.keycloak.adapters.springsecurity.KeycloakConfiguration;
+import org.keycloak.adapters.springsecurity.config.KeycloakWebSecurityConfigurerAdapter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 
-//@Configuration
-//@EnableWebSecurity
-public class SecurityConfig  {
+@KeycloakConfiguration
+public class SecurityConfig extends KeycloakWebSecurityConfigurerAdapter {
 
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.authenticationProvider(keycloakAuthenticationProvider());
+    }
+
+    @Bean
+    @Override
+    protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+        return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+    }
+
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+        http.authorizeRequests()
+                .antMatchers(permitAll()).permitAll()
+                .anyRequest().authenticated();
+    }
 
     protected String[] permitAll() {
         return new String[]{
