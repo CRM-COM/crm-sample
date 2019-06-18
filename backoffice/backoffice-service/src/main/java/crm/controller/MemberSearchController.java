937a2d98c554df393334c4c1fdfdc29b1fd9118f
dhencz <dzshencz@gmail.com>
2019-06-18 12:43:36 +0300

    set content type

--- backoffice/backoffice-service/src/main/java/crm/controller/MemberSearchController.java
+++ backoffice/backoffice-service/src/main/java/crm/controller/MemberSearchController.java
@@ -2,6 +2,8 @@ package crm.controller;
 
 import crm.service.MemberSearchService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -15,11 +17,13 @@ public class MemberSearchController {
     private final MemberSearchService service;
 
     @GetMapping("/member/search")
-    public String search(@RequestParam(name = "criteria", required = false) String criteria,
-                         @RequestParam(name = "query", required = false) String query,
-                         @RequestParam(name = "page", required = false) String page,
-                         @RequestParam(name = "size", required = false) String size,
-                         @RequestParam(name = "sort", required = false) String sort) {
-        return service.search(criteria, query, page, size, sort);
+    public ResponseEntity<String> search(@RequestParam(name = "criteria", required = false) String criteria,
+                                        @RequestParam(name = "query", required = false) String query,
+                                        @RequestParam(name = "page", required = false, defaultValue = "") String page,
+                                        @RequestParam(name = "size", required = false, defaultValue = "") String size,
+                                        @RequestParam(name = "sort", required = false, defaultValue = "") String sort) {
+        return ResponseEntity.ok()
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(service.search(criteria, query, page, size, sort));
     }
 }
