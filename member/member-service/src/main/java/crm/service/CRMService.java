475a28347414f478c79241813af23895e13ab300
dhencz <dzshencz@gmail.com>
2019-06-25 23:29:25 +0300

    fixed response

--- member/member-service/src/main/java/crm/service/CRMService.java
+++ member/member-service/src/main/java/crm/service/CRMService.java
@@ -216,8 +216,13 @@ public class CRMService {
   }
 
   public CRMContactDetails getMember(String crmId) {
+    return Optional.ofNullable(getContactDetailsFromCrm(crmId))
+            .map(CRMContactDetailsResponse::getData).orElse(null);
+  }
+
+  private CRMContactDetailsResponse getContactDetailsFromCrm(String crmId) {
     return restTemplate.getForObject(crmConfig.getUrl() + "/contact_information/show?token=" + getToken() +
-            "&contact_information_identifier=id=" + crmId, CRMContactDetails.class);
+            "&contact_information_identifier=id=" + crmId, CRMContactDetailsResponse.class);
   }
 }
 
