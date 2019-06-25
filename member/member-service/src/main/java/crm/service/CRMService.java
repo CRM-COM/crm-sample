594b2e5d877339e04d3f9c26154bf8d54dfff7d9
dhencz <dzshencz@gmail.com>
2019-06-25 23:18:42 +0300

    fixed url

--- member/member-service/src/main/java/crm/service/CRMService.java
+++ member/member-service/src/main/java/crm/service/CRMService.java
@@ -216,7 +216,7 @@ public class CRMService {
   }
 
   public CRMContactDetails getMember(String crmId) {
-    return restTemplate.getForObject(crmConfig.getUrl() + "contact_information/show?token=" + getToken() +
+    return restTemplate.getForObject(crmConfig.getUrl() + "/contact_information/show?token=" + getToken() +
             "&contact_information_identifier=id=" + crmId, CRMContactDetails.class);
   }
 }
