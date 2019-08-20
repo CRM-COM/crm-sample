2289e13a6e2c103350bc39d2b9fb7dff374f9979
dhencz <dzshencz@gmail.com>
2019-08-20 22:54:10 +0300

    try to install npm and spectacle

--- gateway/swagger-gateway-service/src/main/java/crm/SwaggerGatewayApplication.java
+++ gateway/swagger-gateway-service/src/main/java/crm/SwaggerGatewayApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.PostConstruct;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 @EnableConfigurationProperties
 @SpringBootApplication
@@ -13,5 +19,15 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class SwaggerGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SwaggerGatewayApplication.class, args);
+    }
+
+
+    @PostConstruct
+    public void spectacle() throws IOException, InterruptedException {
+        String commercePath = "http://commerce-service:9017/v2/api-docs";
+        String swaggerJson = new RestTemplate().getForObject(commercePath, String.class);
+        if(swaggerJson == null) return;
+        Files.write(Paths.get("./swagger.json"), swaggerJson.getBytes());
+        Runtime.getRuntime().exec(new String[]{"bash","-c","spectacle swagger.json"}).waitFor();
     }
 }
