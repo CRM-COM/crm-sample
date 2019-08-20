ff3ac7260036f495d63fc297c9d6393d0bebbd2c
dhencz <dzshencz@gmail.com>
2019-08-20 23:33:10 +0300

    added api for spectacle

--- gateway/swagger-gateway-service/src/main/java/crm/SwaggerGatewayApplication.java
+++ gateway/swagger-gateway-service/src/main/java/crm/SwaggerGatewayApplication.java
@@ -1,20 +1,12 @@
 package crm;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.web.client.RestTemplate;
-
-import javax.annotation.PostConstruct;
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 
 
-@Slf4j
 @EnableConfigurationProperties
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -22,17 +14,5 @@ import java.nio.file.Paths;
 public class SwaggerGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(SwaggerGatewayApplication.class, args);
-    }
-
-
-    @PostConstruct
-    public void spectacle() throws IOException, InterruptedException {
-        log.info("Creating spectacle");
-        String commercePath = "http://commerce-service:9017/v2/api-docs";
-        String swaggerJson = new RestTemplate().getForObject(commercePath, String.class);
-        if(swaggerJson == null) return;
-        Files.write(Paths.get("./swagger.json"), swaggerJson.getBytes());
-        Runtime.getRuntime().exec(new String[]{"bash","-c","spectacle swagger.json -t ./src/main/resources/public"}).waitFor();
-        log.info("Created spectacle");
     }
 }
---
+++ gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleController.java
@@ -0,0 +1,28 @@
+package crm.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Slf4j
+@RestController
+@RequestMapping("/spectacle")
+public class SpectacleController {
+
+    @GetMapping()
+    public void refresh() throws IOException, InterruptedException {
+        log.info("Creating spectacle");
+        String commercePath = "http://commerce-service:9017/v2/api-docs";
+        String swaggerJson = new RestTemplate().getForObject(commercePath, String.class);
+        if(swaggerJson == null) return;
+        Files.write(Paths.get("./swagger.json"), swaggerJson.getBytes());
+        Runtime.getRuntime().exec(new String[]{"bash","-c","spectacle swagger.json -t ./src/main/resources/public"}).waitFor();
+        log.info("Created spectacle");
+    }
+}
