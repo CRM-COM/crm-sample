c25a2b0063088f573a36a963e7358920e9960215
dhencz <dzshencz@gmail.com>
2019-08-21 10:58:45 +0300

    try to return html

--- gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleController.java
+++ gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleController.java
@@ -1,9 +1,8 @@
 package crm.controller;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -11,11 +10,10 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Slf4j
-@Controller
-@RequestMapping("/spectacle")
+@RestController("/spectacle")
 public class SpectacleController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping
     public void refresh() throws IOException, InterruptedException {
         log.info("Creating spectacle");
         String commercePath = "http://commerce-service:9017/v2/api-docs";
@@ -24,10 +22,5 @@ public class SpectacleController {
         Files.write(Paths.get("./swagger.json"), swaggerJson.getBytes());
         Runtime.getRuntime().exec(new String[]{"bash", "-c", "spectacle swagger.json -t ./src/main/resources/public -f commerce.html"}).waitFor();
         log.info("Created spectacle");
-    }
-
-    @RequestMapping(method = RequestMethod.GET, path = "/commerce")
-    public String commerce() {
-        return "commerce.html";
     }
 }
