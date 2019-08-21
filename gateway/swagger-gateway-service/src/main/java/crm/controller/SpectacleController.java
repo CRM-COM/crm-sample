75bb5ceb11f9a675c8f8267bcf765d3201204695
dhencz <dzshencz@gmail.com>
2019-08-21 11:27:01 +0300

    try to return html

--- gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleController.java
+++ gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleController.java
@@ -10,7 +10,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 
 @Slf4j
-@RestController("/spectacle")
+@RestController("/refresh")
 public class SpectacleController {
 
     @GetMapping
