af69f3d0eb45cda6c11db88d5254f5e32de08721
dhencz <dzshencz@gmail.com>
2019-08-21 14:54:08 +0300

    try to return html

--- gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleService.java
+++ gateway/swagger-gateway-service/src/main/java/crm/controller/SpectacleService.java
@@ -13,14 +13,14 @@ import java.nio.file.Paths;
 @Service
 public class SpectacleService {
 
-    @PostConstruct
+//    @PostConstruct
     public void refresh() throws IOException, InterruptedException {
         log.info("Creating spectacle");
         String commercePath = "http://commerce-service:9017/v2/api-docs";
         String swaggerJson = new RestTemplate().getForObject(commercePath, String.class);
         if (swaggerJson == null) return;
         Files.write(Paths.get("./swagger.json"), swaggerJson.getBytes());
-        Runtime.getRuntime().exec(new String[]{"bash", "-c", "spectacle swagger.json -t ./src/main/resources/templates -f commerce.html"}).waitFor();
+        Runtime.getRuntime().exec(new String[]{"bash", "-c", "spectacle swagger.json -t ./src/main/resources/commerce -f commerce.html"}).waitFor();
         log.info("Created spectacle");
     }
 }
